532d0b11ace459d736321fd446f961eca07ac279
omelchenkoaleks <omelchenkoaleks@gmail.com>
2021-07-09 17:40:23 +0300

    Initial commit

---
+++ app/src/main/java/com/omelchenkoaleks/notificationserviceexample/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.omelchenkoaleks.notificationserviceexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.omelchenkoaleks.notificationserviceexample.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity(), LifecycleObserver {
+
+    private lateinit var binding: ActivityMainBinding
+    private var startTime = 0L
+    private var job: Job? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        startTime = System.currentTimeMillis()
+
+        job = GlobalScope.launch(Dispatchers.Main) {
+            while (true) {
+                binding.timerView.text = (System.currentTimeMillis() - startTime).displayTime()
+                delay(INTERVAL)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job?.cancel()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onAppBackgrounded() {
+        val startIntent = Intent(this, ForegroundService::class.java)
+        startIntent.putExtra(COMMAND_ID, COMMAND_START)
+        startIntent.putExtra(STARTED_TIMER_TIME_MS, startTime)
+        startService(startIntent)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onAppForegrounded() {
+        val stopIntent = Intent(this, ForegroundService::class.java)
+        stopIntent.putExtra(COMMAND_ID, COMMAND_STOP)
+        startService(stopIntent)
+    }
+
+    private companion object {
+        private const val INTERVAL = 10L
+    }
+
+}
